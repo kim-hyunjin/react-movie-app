061c001fb245b31d4c68d056d8d3d65f06c95147
Kim HyunJin <hj.kim@bono.tech>
2020-08-10 17:43:57 +0900

    JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Potato from "./Potato";
+
+function Food({fav}) {
+    return <h1>I like {fav}</h1>
+}
 
 function App() {
   return (
       <div className="App">
         <h1>Hello</h1>
-        <Potato />
+          <Food fav="kimchi"/>
       </div>
 
   );
